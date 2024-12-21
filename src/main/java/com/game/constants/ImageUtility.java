935778c9aa50bf6ae7251f16d515244559bcfdb1
Shubhambhagat3226 <shubhambhagat3220@gmail.com>
2024-12-21 18:51:46 +0530

    add image of oldman npc in imageutility

--- src/main/java/com/game/constants/ImageUtility.java
+++ src/main/java/com/game/constants/ImageUtility.java
@@ -2,57 +2,68 @@ package com.game.constants;
 
 public class ImageUtility {
     // PLAYER IMAGES
-    public static final String  PLAYER_UP_1 = "/Player/Walking sprites/boy_up_1.png";
-    public static final String PLAYER_UP_2 = "/Player/Walking sprites/boy_up_2.png";
-    public static final String PLAYER_DOWN_1 = "/Player/Walking sprites/boy_down_1.png";
-    public static final String PLAYER_DOWN_2 = "/Player/Walking sprites/boy_down_2.png";
-    public static final String PLAYER_LEFT_1 = "/Player/Walking sprites/boy_left_1.png";
-    public static final String PLAYER_LEFT_2 = "/Player/Walking sprites/boy_left_2.png";
+    public static final String  PLAYER_UP_1   = "/Player/Walking sprites/boy_up_1.png";
+    public static final String PLAYER_UP_2    = "/Player/Walking sprites/boy_up_2.png";
+    public static final String PLAYER_DOWN_1  = "/Player/Walking sprites/boy_down_1.png";
+    public static final String PLAYER_DOWN_2  = "/Player/Walking sprites/boy_down_2.png";
+    public static final String PLAYER_LEFT_1  = "/Player/Walking sprites/boy_left_1.png";
+    public static final String PLAYER_LEFT_2  = "/Player/Walking sprites/boy_left_2.png";
     public static final String PLAYER_RIGHT_1 = "/Player/Walking sprites/boy_right_1.png";
     public static final String PLAYER_RIGHT_2 = "/Player/Walking sprites/boy_right_2.png";
 
+    // NPC
+    public static final String OLD_MAN_UP_1    = "/NPC/oldman_up_1.png";
+    public static final String OLD_MAN_UP_2    = "/NPC/oldman_up_2.png";
+    public static final String OLD_MAN_DOWN_1  = "/NPC/oldman_down_1.png";
+    public static final String OLD_MAN_DOWN_2  = "/NPC/oldman_down_2.png";
+    public static final String OLD_MAN_LEFT_1  = "/NPC/oldman_left_1.png";
+    public static final String OLD_MAN_LEFT_2  = "/NPC/oldman_left_2.png";
+    public static final String OLD_MAN_RIGHT_1 = "/NPC/oldman_right_1.png";
+    public static final String OLD_MAN_RIGHT_2 = "/NPC/oldman_right_2.png";
+
+
     // TILE IMAGES
-    public static final String GRASS_0 = "/Tiles/New version/grass00.png";
-    public static final String GRASS_1 = "/Tiles/New version/grass01.png";
-    public static final String WALL = "/Tiles/New version/wall.png";
-    public static final String WATER_0 = "/Tiles/New version/water00.png";
-    public static final String WATER_1 = "/Tiles/New version/water01.png";
-    public static final String WATER_2 = "/Tiles/New version/water02.png";
-    public static final String WATER_3 = "/Tiles/New version/water03.png";
-    public static final String WATER_4 = "/Tiles/New version/water04.png";
-    public static final String WATER_5 = "/Tiles/New version/water05.png";
-    public static final String WATER_6 = "/Tiles/New version/water06.png";
-    public static final String WATER_7 = "/Tiles/New version/water07.png";
-    public static final String WATER_8 = "/Tiles/New version/water08.png";
-    public static final String WATER_9 = "/Tiles/New version/water09.png";
+    public static final String GRASS_0  = "/Tiles/New version/grass00.png";
+    public static final String GRASS_1  = "/Tiles/New version/grass01.png";
+    public static final String WALL     = "/Tiles/New version/wall.png";
+    public static final String WATER_0  = "/Tiles/New version/water00.png";
+    public static final String WATER_1  = "/Tiles/New version/water01.png";
+    public static final String WATER_2  = "/Tiles/New version/water02.png";
+    public static final String WATER_3  = "/Tiles/New version/water03.png";
+    public static final String WATER_4  = "/Tiles/New version/water04.png";
+    public static final String WATER_5  = "/Tiles/New version/water05.png";
+    public static final String WATER_6  = "/Tiles/New version/water06.png";
+    public static final String WATER_7  = "/Tiles/New version/water07.png";
+    public static final String WATER_8  = "/Tiles/New version/water08.png";
+    public static final String WATER_9  = "/Tiles/New version/water09.png";
     public static final String WATER_10 = "/Tiles/New version/water10.png";
     public static final String WATER_11 = "/Tiles/New version/water11.png";
     public static final String WATER_12 = "/Tiles/New version/water12.png";
     public static final String WATER_13 = "/Tiles/New version/water13.png";
 
-    public static final String ROAD_0 = "/Tiles/New version/road00.png";
-    public static final String ROAD_1 = "/Tiles/New version/road01.png";
-    public static final String ROAD_2 = "/Tiles/New version/road02.png";
-    public static final String ROAD_3 = "/Tiles/New version/road03.png";
-    public static final String ROAD_4 = "/Tiles/New version/road04.png";
-    public static final String ROAD_5 = "/Tiles/New version/road05.png";
-    public static final String ROAD_6 = "/Tiles/New version/road06.png";
-    public static final String ROAD_7 = "/Tiles/New version/road07.png";
-    public static final String ROAD_8 = "/Tiles/New version/road08.png";
-    public static final String ROAD_9 = "/Tiles/New version/road09.png";
+    public static final String ROAD_0  = "/Tiles/New version/road00.png";
+    public static final String ROAD_1  = "/Tiles/New version/road01.png";
+    public static final String ROAD_2  = "/Tiles/New version/road02.png";
+    public static final String ROAD_3  = "/Tiles/New version/road03.png";
+    public static final String ROAD_4  = "/Tiles/New version/road04.png";
+    public static final String ROAD_5  = "/Tiles/New version/road05.png";
+    public static final String ROAD_6  = "/Tiles/New version/road06.png";
+    public static final String ROAD_7  = "/Tiles/New version/road07.png";
+    public static final String ROAD_8  = "/Tiles/New version/road08.png";
+    public static final String ROAD_9  = "/Tiles/New version/road09.png";
     public static final String ROAD_10 = "/Tiles/New version/road10.png";
     public static final String ROAD_11 = "/Tiles/New version/road11.png";
     public static final String ROAD_12 = "/Tiles/New version/road12.png";
 
-    public static final String SAND = "/Tiles/Old version/sand.png";
-    public static final String TREE = "/Tiles/New version/tree.png";
+    public static final String SAND   = "/Tiles/Old version/sand.png";
+    public static final String TREE   = "/Tiles/New version/tree.png";
     public static final String  EARTH = "/Tiles/New version/earth.png";
 
     // OBJECTS IMAGES
-    public static final String KEY = "/Object/key.png";
-    public static final String CHEST = "/Object/chest.png";
+    public static final String KEY         = "/Object/key.png";
+    public static final String CHEST       = "/Object/chest.png";
     public static final String WOODEN_DOOR = "/Object/door.png";
-    public static final String BOOTS = "/Object/boots.png";
-    public static final String IRON_DOOR = "/Object/door_iron.png";
+    public static final String BOOTS       = "/Object/boots.png";
+    public static final String IRON_DOOR   = "/Object/door_iron.png";
 
 }
